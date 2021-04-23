71f0db0f6853ead7f8d26982d3396b1409bad044
Frankium <tis7bfrankie@gmail.com>
2021-04-23 20:24:55 +0800

    moved from old project

---
+++ src/components/Header.vue
@@ -0,0 +1,78 @@
+<template>
+  <cv-header id="header-bar" aria-label="Carbon tutorial">
+    <cv-skip-to-content href="#main-menu">Skip to content</cv-skip-to-content>
+    <cv-header-name id="header-name" href="/">
+      <div id="vel-name">LingaVëlhëlkaza Elvorkalesha</div>
+      <div id="foreign-names">Dictionary of the Valhallas Language 維赫語詞典</div>
+    </cv-header-name>
+    <cv-header-nav>
+      <!-- <cv-header-menu-item href="/repos">Repositories</cv-header-menu-item> -->
+      <cv-text-input
+        id="word-search"
+        invalidText="A valid value is required"
+        labelText="Text input label"
+        :placeholder=searchPlaceholder
+      />
+      <cv-dropdown
+        ariaLabel="Dropdown"
+        id="search-language-options"
+        :items=searchLanguageOptions
+        titleText="Dropdown title"
+      />
+    </cv-header-nav>
+    <template slot="header-global">
+      <cv-header-global-action :class=displayLanguageSelection[0] id="display-language-eng">ENG</cv-header-global-action>
+      <cv-header-global-action :class=displayLanguageSelection[1] id="display-language-zhtw">繁中</cv-header-global-action>
+      <cv-header-global-action :class=displayLanguageSelection[2] id="display-language-zhcn">简中</cv-header-global-action>
+      <cv-header-global-action :class=displayLanguageSelection[3] id="display-language-vel">VËL</cv-header-global-action>
+    </template>
+  </cv-header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {},
+  data(){return{
+    searchPlaceholder: "Enter a Word",
+    searchLanguageOptions: ["Valhallas to English","Valhallas to Traditional Chinese","Valhallas to Simplified Chinese","Chinese to Valhallas","English to Valhallas"],
+    displayLanguageSelection:["selected display-language-options","display-language-options","display-language-options","display-language-options"]
+  }}
+};
+</script>
+
+<style scoped lang="scss">
+@import "../styles/theme.scss";
+.display-language-options{
+  height:57px;
+  width:57px;
+  color: $fg-sub;
+}
+.display-language-options.selected{
+  color: $fg-main;
+}
+#search-language-options{
+  padding-top:8px;
+  padding-left:8px;
+}
+#header-bar{
+  height:57px;
+  background: $bg-dark;
+}
+#header-name{
+  padding: 5px;
+  display:block;
+  margin-top:auto;
+  // padding-top:2px;
+}
+#header-name>#vel-name{
+  font-family: "Marcellus", serif;
+  font-size: 16px;
+}
+#header-name>#foreign-names{
+  font-family: "Roboto", sans-serif;
+  font-size: 11px;
+  color: $fg-sub;
+  font-weight: normal;
+}
+</style>
