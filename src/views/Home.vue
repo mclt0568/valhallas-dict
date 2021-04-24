03cf9e631672ea36f54dc5865800d410ff96cf37
Frankium <tis7bfrankie@gmail.com>
2021-04-24 22:09:21 +0800

    add home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,83 @@
 <template>
-  <div class="home">
+  <div id="home">
+    <cv-grid kind="condensed" id="menu-grid">
+      <cv-row>
+        <cv-column :sm="0" :md="1" :lg="3"></cv-column>
+        <cv-column :sm="4" :md="3" :lg="3">
+          <div class="cv-grid-story__preview-col full">
+            <div class="button-column full">
+              <ButtonWrapper type="main">{{texts["list"]}}<Catalog32/></ButtonWrapper>
+            </div>
+          </div>
+        </cv-column>
+        <cv-column :sm="4" :md="3" :lg="3">
+          <div class="cv-grid-story__preview-col">
+            <div class="button-column sub">
+              <ButtonWrapper>{{texts["bookmark"]}}<Bookmark32/></ButtonWrapper>
+              <ButtonWrapper>{{texts["learn"]}}<Idea32/></ButtonWrapper>
+              <ButtonWrapper>{{texts["about"]}}<Information32/></ButtonWrapper>
+            </div>
+          </div>
+        </cv-column>
+        <cv-column :sm="0" :md="1" :lg="3"></cv-column>
+      </cv-row>
+    </cv-grid>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import ButtonWrapper from "@/views/Home/ButtonWrapper";
+import {Bookmark32, Catalog32, Idea32, Information32} from "@carbon/icons-vue";
 
 export default {
   name: 'Home',
   components: {
-  }
+    ButtonWrapper,
+    Catalog32,
+    Information32,
+    Idea32,
+    Bookmark32
+  },
+  data(){return{
+    texts:{
+      "list":"List of Valhallas Words",
+      "bookmark":"Bookmarked Pages",
+      "learn":"Learn Valhallas Language",
+      "about":"About",
+    }
+  }}
 }
 </script>
+
+<style lang="scss" scoped>
+@media only screen and (max-width: 672px) {
+  .button-column.full{
+    padding-top:1.5rem;
+  }
+  .button-column{
+    padding:0 1.5rem;
+  }
+}
+@media only screen and (min-width: 672px) {
+  #home{
+    display:grid;
+    place-items: center;
+  }
+}
+#home{
+  height: calc(100vh - 3.5rem);
+}
+#menu-grid{
+  width: 100%;
+}
+.full{
+  height:100%;
+}
+.buttom-column{
+  >button{
+    max-width: none;
+    width:100%;
+  }
+}
+</style>
---
+++ src/views/Home/ButtonWrapper.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="button-wrapper" :class=type>
+    <cv-button :renderIcon=renderIcon class="menu-button" kind='secondary'>
+      <slot></slot>
+    </cv-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonWrapper',
+  components: {},
+  props:[
+    "type",
+    "renderIcon"
+  ]
+}
+</script>
+
+<style lang="scss" scoped>
+.button-wrapper{
+  padding:4px;
+
+  >button{
+    max-width: none;
+    width:100%;
+    height:100%;
+  }
+}
+.main button{
+  vertical-align: bottom;
+}
+.main, .main div{
+  height:100%
+}
+</style>
