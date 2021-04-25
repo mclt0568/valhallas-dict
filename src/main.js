167cfb52b768dc1e24db2cd09dc6e86084a03a07
Frankium <tis7bfrankie@gmail.com>
2021-04-25 14:41:37 +0800

    add i18n to json package

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import CarbonComponentsVue from "@carbon/vue";
 import 'carbon-components/css/carbon-components.css';
+import VueI18n from 'vue-i18n'
 
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(CarbonComponentsVue);
+Vue.use(VueI18n);
 
 new Vue({
   router,
